475c07c4314d6fe285736699cc68184f42893175
Umut Yenidil <umut.yenidil9@gmail.com>
2026-01-11 15:01:09 +0300

    feat: add a reactive endpoint '/api/v1/chat/stream' to return an ai response in flux type.

--- src/main/java/com/umutyenidil/springaidemo/chat/ChatController.java
+++ src/main/java/com/umutyenidil/springaidemo/chat/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/v1/chat")
@@ -20,6 +21,14 @@ public class ChatController {
         return chatClient.prompt()
                 .user("Tell me an interesting fact about the programming language named Java.")
                 .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream() {
+        return chatClient.prompt()
+                .user("I'm visiting Hilton Head soon, can you give me 10 places I must visit")
+                .stream()
                 .content();
     }
 }
